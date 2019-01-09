5b268d114911c08602a80fb20d2d0ee70e4e9374
celestinosalim <celestino.salim@gmail.com>
2019-01-09 17:33:44 -0500

    added expose and maxge to cors

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -10,6 +10,8 @@ Rails.application.config.middleware.insert_before 0, Rack::Cors do
     origins 'https://cook-time.herokuapp.com'
     resource '*',
       headers: :any,
-      methods: [:get, :post, :put, :patch, :delete, :options, :head]
+      methods: [:get, :post, :put, :patch, :delete, :options, :head],
+      expose: ['access-token', 'expiry', 'token-type', 'uid', 'client'],
+      max_age: 0
   end
 end
