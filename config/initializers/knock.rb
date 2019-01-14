2aa1e6d2ba993380b95dedbe5d2e0525d4908d73
celestinosalim <celestino.salim@gmail.com>
2019-01-14 13:30:08 -0500

    to_S

--- config/initializers/knock.rb
+++ config/initializers/knock.rb
@@ -2,9 +2,9 @@ Knock.setup do |config|
 
   config.token_lifetime = 1.week
   config.token_signature_algorithm = 'HS256'
-  config.token_secret_signature_key = -> { Rails.application.secrets.secret_key_base }
+  config.token_secret_signature_key = -> { Rails.application.secrets.secret_key_base.to_s }
   config.not_found_exception_class_name = 'ActiveRecord::RecordNotFound'
-  config.token_secret_signature_key = -> { Rails.application.credentials.secret_key_base }
+  config.token_secret_signature_key = -> { Rails.application.credentials.secret_key_base.to_s }
 
   ## Expiration claim
   ## ----------------
